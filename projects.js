3d87d2128784f654d0f675218cf80fda0e5dc514
Shivansh Modawal <smodaw2@uic.edu>
2021-04-11 02:41:36 +0000

    final_commit

--- projects.js
+++ projects.js
@@ -1,18 +1,20 @@
 let projects = [
 	{
 		"Name":"locationAwareMap",
-		"Discription":"Shows the loaction and the co-ordinates on the user using google maps API"
-		"Language Used":"HTML"
-		"Link to Repo":"https://github.com/ShivanshModawal/IT202-Spring2021-activities.git"
-		"Link to Page":"https://shivanshmodawal.github.io/IT202-Spring2021-activities/locationAwareMap.html"
+		"Discription":"Shows the loaction and the co-ordinates on the user using google maps API",
+		"img":"a.jpg",
+		"Language":"HTML",
+		"Repo":"https://github.com/ShivanshModawal/IT202-Spring2021-activities.git",
+		"Page":"https://shivanshmodawal.github.io/IT202-Spring2021-activities/locationAwareMap.html"
 	},
 	
 	{
 		"Name":"chart_basics",
-		"Discription":"Displays Different types of charts usind google charts API"
-		"Language Used":"HTML"
-		"Link to Repo":"https://github.com/ShivanshModawal/IT202-Spring2021-activities.git"
-		"Link to Page":"https://shivanshmodawal.github.io/IT202-Spring2021-activities/chart_basics.html"
+		"Discription":"Displays Different types of charts usind google charts API",
+		"img":"b.jpg",
+		"Language":"HTML",
+		"Repo":"https://github.com/ShivanshModawal/IT202-Spring2021-activities.git",
+		"Page":"https://shivanshmodawal.github.io/IT202-Spring2021-activities/chart_basics.html"
 	}
 	
 ];
